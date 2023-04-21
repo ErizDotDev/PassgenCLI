2695c168b44e5166a225fbc2f431d540e1ba783a
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-04-22 02:05:36 +0800

    add core library for password generation

--- src/DPG.CLI/Program.cs
+++ src/DPG.CLI/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace DPG.CLI;
+﻿using DPG.Core;
+
+namespace DPG.CLI;
 
 internal class Program
 {
    static void Main(string[] args)
    {
+      var passwordGenerator = new PasswordGenerator(args);
+
       if (args.Length == 0)
       {
          Console.WriteLine("dpg <command>");
---
+++ src/DPG.Core/PasswordGenerator.cs
@@ -0,0 +1,21 @@
+﻿namespace DPG.Core;
+
+public class PasswordGenerator
+{
+   private string[] commandLineArgs;
+
+   public PasswordGenerator(string[] args)
+   {
+      commandLineArgs = args;
+   }
+
+   public string Generate()
+   {
+      if (commandLineArgs.Length == 0)
+      {
+         Console.WriteLine("dpg <command>");
+      }
+
+      return string.Empty;
+   }
+}
