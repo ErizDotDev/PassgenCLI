2d18b6a119383c4a060bf37d9b7fe9e5beb46bc3
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-04-22 20:19:08 +0800

    identify and execute help function

--- src/DPG.Core/PasswordGenerator.cs
+++ src/DPG.Core/PasswordGenerator.cs
@@ -6,6 +6,9 @@ public class PasswordGenerator
 {
     private string[] commandLineArgs;
 
+    private const string HELP_ALIAS = "-h";
+    private const string HELP_COMMAND_TEXT = "help";
+
     public PasswordGenerator(string[] args)
     {
         commandLineArgs = args;
@@ -14,6 +17,11 @@ public class PasswordGenerator
     public string Generate()
     {
         if (commandLineArgs.Length == 0)
+        {
+            return ShowHelpMessage();
+        }
+
+        if (commandLineArgs.Contains(HELP_ALIAS) || commandLineArgs.Contains(HELP_COMMAND_TEXT))
         {
             return ShowHelpMessage();
         }
