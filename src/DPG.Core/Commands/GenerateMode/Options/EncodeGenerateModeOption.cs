4a1b9bc1d6d6b10f3cd20086ce5e053d6d5a7144
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-05-28 23:08:22 +0800

    complete transfer of code to option classes

--- src/DPG.Core/Commands/GenerateMode/Options/EncodeGenerateModeOption.cs
+++ src/DPG.Core/Commands/GenerateMode/Options/EncodeGenerateModeOption.cs
@@ -1,8 +1,93 @@
-﻿namespace DPG.Core.Commands.GenerateMode.Options;
+﻿using System.Text;
 
-internal class EncodeGenerateModeOption
+namespace DPG.Core.Commands.GenerateMode.Options;
+
+internal class EncodeGenerateModeOption : IGenerateModeOption
 {
     public const string Name = "encode";
 
     public const string Description = "encodes a provided pass phrase with password safe characters";
+
+    private Dictionary<string, string> Flags = new Dictionary<string, string>();
+
+    public EncodeGenerateModeOption()
+    {
+        Flags = LoadFlags();
+    }
+
+    public void Execute(string[] arguments)
+    {
+        if (arguments.Contains(HelpCommand.Alias))
+            ShowHelpMessage();
+
+        try
+        {
+            var passwordGenerator = new EncodePasswordGenerator() as IPasswordGenerator;
+
+            var passPhrase = arguments[0];
+            if (string.IsNullOrEmpty(passPhrase))
+            {
+                Console.WriteLine("Provide a valid pass phrase");
+                return;
+            }
+
+            var encodeModeOptions = new PasswordGeneratorEncodeOptions()
+            {
+                PassPhrase = passPhrase,
+                SubstituteSpacesWithUnderscores = arguments.Contains(Flags["SubSpaces"])
+            };
+
+            var generatedPassword = passwordGenerator.GeneratePassword(encodeModeOptions);
+
+            Console.WriteLine($"Generated password is:\n{generatedPassword}");
+        }
+        catch (IndexOutOfRangeException)
+        {
+            ShowHelpMessage();
+        }
+        catch
+        {
+            Console.WriteLine("Something went wrong with the application.");
+        }
+    }
+
+    private Dictionary<string, string> LoadFlags()
+    {
+        return new Dictionary<string, string>()
+        {
+            { "SubSpaces", "--substitute-whitespace-with-underscore" }
+        };
+    }
+
+    private void ShowHelpMessage()
+    {
+        var helpMessage = BuildMessageContent();
+        Console.WriteLine(helpMessage);
+    }
+
+    private string BuildMessageContent()
+    {
+        var helpMessageBuilder = new StringBuilder();
+        var flagsContent = GetAvailableFlagsForDisplay();
+
+        helpMessageBuilder.AppendLine()
+            .AppendLine("usage: dpg mode standard [pass phrase] [arguments]")
+            .AppendLine()
+            .AppendLine("Available options:\n")
+            .AppendLine(flagsContent);
+
+        return helpMessageBuilder.ToString();
+    }
+
+    private string GetAvailableFlagsForDisplay()
+    {
+        var flagsMessageBuilder = new StringBuilder();
+
+        foreach (var flag in Flags)
+        {
+            Console.WriteLine($"  {flag.Value}");
+        }
+
+        return flagsMessageBuilder.ToString();
+    }
 }
--- src/DPG.Core/Commands/GenerateMode/Options/StandardGenerateModeOption.cs
+++ src/DPG.Core/Commands/GenerateMode/Options/StandardGenerateModeOption.cs
@@ -8,27 +8,115 @@ internal class StandardGenerateModeOption : IGenerateModeOption
 
     public const string Description = "generates a random password";
 
+    private Dictionary<string, string> Flags = new Dictionary<string, string>();
+
+    private const int DEFAULT_PASSWORD_LENGTH = 16;
+
+    public StandardGenerateModeOption()
+    {
+        Flags = LoadFlags();
+    }
+
     public void Execute(string[] arguments)
     {
         if (arguments.Contains(HelpCommand.Alias))
             ShowHelpMessage();
+
+        try
+        {
+            var passwordLength = 0;
+            var passwordGenerator = new StandardPasswordGenerator() as IPasswordGenerator;
+
+            if (arguments.Contains(Flags["PasswordLength"]))
+            {
+                var passwordLengthArgIndex = Array.IndexOf(arguments, Flags["PasswordLength"]);
+                passwordLength = GetPasswordLength(arguments, passwordLengthArgIndex);
+            }
+            else
+            {
+                passwordLength = DEFAULT_PASSWORD_LENGTH;
+            }
+
+            if (passwordLength < 1)
+            {
+                Console.WriteLine("Error: Invalid password length provided");
+                return;
+            }
+
+            var standardModeOptions = new PasswordGeneratorStandardOptions
+            {
+                PasswordLength = passwordLength,
+                EnableUppercaseCharacters = !arguments.Contains(Flags["DisableUppercase"]),
+                EnableLowercaseCharacters = !arguments.Contains(Flags["DisableLowercase"]),
+                EnableNumericCharacters = !arguments.Contains(Flags["DisableNumerals"]),
+                EnableSpecialCharacters = !arguments.Contains(Flags["DisableSpecialChars"]),
+            };
+
+            var generatedPassword = passwordGenerator.GeneratePassword(standardModeOptions);
+
+            Console.WriteLine($"Generated password is:\n{generatedPassword}");
+        }
+        catch
+        {
+            Console.WriteLine("Something went wrong with the application!");
+        }
     }
 
     private void ShowHelpMessage()
     {
         var helpMessage = BuildMessageContent();
-
         Console.WriteLine(helpMessage);
     }
 
     private string BuildMessageContent()
     {
         var helpMessageBuilder = new StringBuilder();
+        var flagsContent = GetAvailableFlagsForDisplay();
 
         helpMessageBuilder.AppendLine()
             .AppendLine("usage: dpg mode standard [arguments]")
-            .AppendLine();
+            .AppendLine()
+            .AppendLine("Available options:\n")
+            .AppendLine(flagsContent);
 
         return helpMessageBuilder.ToString();
+    }
+
+    private Dictionary<string, string> LoadFlags()
+    {
+        return new Dictionary<string, string>()
+        {
+            { "PasswordLength", "--password-length" },
+            { "DisableUppercase", "--disable-uppercase-chars" },
+            { "DisableLowercase", "--disable-lowercase-chars" },
+            { "DisableNumerals", "--disable-numeric-chars" },
+            { "DisableSpecialChars", "--disable-special-chars" }
+        };
+    }
+
+    private int GetPasswordLength(string[] arguments, int passwordLengthOptionIndex)
+    {
+        Console.WriteLine(passwordLengthOptionIndex);
+
+        var passwordLength = DEFAULT_PASSWORD_LENGTH;
+
+        if (!int.TryParse(arguments[passwordLengthOptionIndex + 1], out passwordLength))
+        {
+            return -1;
+        }
+
+        return passwordLength;
+    }
+
+    private string GetAvailableFlagsForDisplay()
+    {
+        var flagsMessageBuilder = new StringBuilder();
+
+        foreach (var flag in Flags)
+        {
+            Console.WriteLine($"  {flag.Value}");
+        }
+
+        return flagsMessageBuilder.ToString();
     }
 }
